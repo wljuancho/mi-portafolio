f8a21595fa8709bbf5170b74f3a076dfde923987
GurgoSoft <Gurgosoft@gmail.com>
2026-04-16 10:19:28 -0500

    se modifica de tal forma que sea real osea que no tengo informacion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,27 +6,13 @@ import Contacto from "./components/Contacto";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const misProyectos = [
-  {
-    name: "FinTech App",
-    tecnologias: ["React", "Firebase"],
-    description: "Plataforma para gestión de gastos personales con gráficos en tiempo real.",
-    image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=800",
-    url: "https://github.com/tu-usuario/proyecto1"
-  },
-  {
-    name: "Jank School",
-    tecnologias: ["Java", "Python"],
-    description: "Sistema de gestión escolar desarrollado para optimizar el registro de notas.",
-    image: "https://images.unsplash.com/photo-1509062522246-3755977927d7?auto=format&fit=crop&q=80&w=800",
-    url: "https://tu-sitio-demo.com"
-  }
-  ];
+  
+  
   const misTecnologias = [
-  { name: 'HTML', level: 90 },
-  { name: 'Python', level: 75 },
-  { name: 'Java', level: 70 },
-  { name: 'React', level: 85 }
+  { name: 'HTML', level: 50 },
+  { name: 'Python', level: 65 },
+  { name: 'Java', level: 40 },
+  { name: 'React', level: 30 }
   ]
   return (
     <div>
@@ -39,7 +25,7 @@ function App() {
 
       <Tecnologias tecnolo={misTecnologias} />
 
-      <Proyecto Proyectos={misProyectos} />
+     
       <Contacto Telefono = {573012758519} Email="juanangelquinteroroldan82@gmail.com"
       Github="wljuancho"/>
       
